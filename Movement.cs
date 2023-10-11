df7907fdec59573810ed2a9378e78bf5e94b1581
희수 <79899382+ekqlcl589@users.noreply.github.com>
2023-10-11 17:57:10 +0900

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -9,7 +9,7 @@ public class MoveMent : MonoBehaviour
 
     public Transform target;
     private const float moveSpeed = 1f;
-    private const float distance = 1f;
+    private const float distance = 3f;
 
     private const float zeroPoint = 0f;
     void Start()
@@ -20,35 +20,44 @@ public class MoveMent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
-            Move();
+        Move();
     }
 
-    public void Move()
+    private void Move()
     {
-        // 방향 벡터
-        Vector3 direction = target.position - transform.position;
-
-        direction.y = zeroPoint;
-
-        // 타겟을 향해 회전
-        Quaternion rotation = Quaternion.LookRotation(direction.normalized);
-        transform.rotation = rotation;
-
-        //타겟과의 거리 계산
-        float distanceToPosition = Vector3.Distance(transform.position, target.position);
-
-        // 타겟과의 거리가 distance 보다 크다면 이동 
-        if (distanceToPosition > distance)
+        if (target != null)
         {
-            Vector3 move = direction.normalized * moveSpeed * Time.deltaTime;
-            transform.position += move;
+            // 방향 벡터
+            Vector3 direction = target.position - transform.position;
+
+            direction.y = zeroPoint;
+
+            // 타겟을 향해 회전
+            Quaternion rotation = Quaternion.LookRotation(direction.normalized);
+            transform.rotation = rotation;
+
+            //타겟과의 거리 계산
+            float distanceToPosition = Vector3.Distance(transform.position, target.position);
+
+            // 타겟과의 거리가 distance 보다 크다면 이동 
+            if (distanceToPosition > distance)
+            {
+                Vector3 move = direction.normalized * moveSpeed * Time.deltaTime;
+                transform.position += move;
+            }
+            else
+            {
+                //공격
+            }
         }
     }
 
     //다른 타겟으로 변경할 때 사용할 함수 
     public void SetTarget(Transform newTarget)
     {
+        if (newTarget == null && gameObject == null)
+            return;
+
         target = newTarget;
     }
 
