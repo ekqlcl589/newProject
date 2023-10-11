3d05b3d83601f3b5358a68f28492598abd516903
희수 <79899382+ekqlcl589@users.noreply.github.com>
2023-10-11 18:51:14 +0900

    fix

---
+++ Constant.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constant
+{
+    public const float MAX_HP = 100f;
+    public const float DIE_HP = 0f;
+
+    public const float MOVE_SPEED = 1f;
+    public const float DISTANCE = 3f;
+
+    public const float ZERO = 0f;
+    public const float ZERO_point = 0f;
+
+    public const float DAMAGE = 10f;// 데미지 상수?
+
+    public const float ATTACK_COLLTIME = 1f;
+
+}
--- Movement.cs
+++ Movement.cs
@@ -5,17 +5,7 @@ using UnityEngine.AI;
 
 public class MoveMent : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     public Transform target;
-    private const float moveSpeed = 1f;
-    private const float distance = 3f;
-
-    private const float zeroPoint = 0f;
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
@@ -30,7 +20,7 @@ public class MoveMent : MonoBehaviour
             // 방향 벡터
             Vector3 direction = target.position - transform.position;
 
-            direction.y = zeroPoint;
+            direction.y = Constant.ZERO_point;
 
             // 타겟을 향해 회전
             Quaternion rotation = Quaternion.LookRotation(direction.normalized);
@@ -40,9 +30,9 @@ public class MoveMent : MonoBehaviour
             float distanceToPosition = Vector3.Distance(transform.position, target.position);
 
             // 타겟과의 거리가 distance 보다 크다면 이동 
-            if (distanceToPosition > distance)
+            if (distanceToPosition > Constant.DISTANCE)
             {
-                Vector3 move = direction.normalized * moveSpeed * Time.deltaTime;
+                Vector3 move = direction.normalized * Constant.MOVE_SPEED * Time.deltaTime;
                 transform.position += move;
             }
             else
