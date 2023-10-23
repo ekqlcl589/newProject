022945f368c30ebedcf5f5583bd689308eacead5
heesu jo <heesu8313@gmail.com>
2023-10-24 01:20:53 +0900

    fork 커밋 테스트2

--- BulletShooter.cs
+++ BulletShooter.cs
@@ -17,7 +17,6 @@ public class BulletShooter : MonoBehaviour
         // BulletShooter 의 목적은 원본으로 만들어 둔 Bullet의 Prefab을 통해 사본을 생성하는 역할을 하기 때문에 
         // 다른 조건 없이 shot 함수를 통해 다른 조건 없이 사본을 생성하게 설계 
         Bullet bullet = Instantiate(bulletPrefab, bulletPoint.position, bulletPoint.rotation);
-
     }
 
 }
