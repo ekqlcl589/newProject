cd2847141cce1abec72febe77cd2152f41613a7f
희수 <79899382+ekqlcl589@users.noreply.github.com>
2023-10-24 12:00:55 +0900

    Bullet 프리팹 조건 수정 및 Shooter 주석 정리

--- Bullet.cs
+++ Bullet.cs
@@ -29,23 +29,22 @@ public class Bullet : MonoBehaviour
 
     private void Start()
     {
-        // 초기 위치값을 MonoBehaviour 클래스가 초기화 될 때의 위치값을 대입
+        // 거리 비례 삭제 시 계산에 필요한 초기 위치값을 MonoBehaviour 클래스가 초기화 될 때의 위치값을 대입
         startPosition = transform.position;
         // bulletDamage 변수에 Constant.DAMAGE(10f) 값 대입
         bulletDamage = Constant.DAMAGE;
-        // 거리가 멀어지면 삭제시키는 코루틴 시작
+        // 코루틴 메서드가 동작(true)중 인지 아닌지(false) 구분하기 위해 값 대입
         DeleteByDistanceCoroutine = StartCoroutine(DeleteByDistance());
     }
     
     private void FixedUpdate()
     {
-        // rigidBody의 AddForce 로 힘을 가해주는 기능을 하는 함수를 호출
         BulletMove();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // 충돌 시 오브젝트를 삭제시키는 함수 실행
+        // bullet 은 충돌 시 추가 충돌을 막기 위해 삭제
         Delete();
     }
 
@@ -54,39 +53,35 @@ public class Bullet : MonoBehaviour
         // DeleteByDistanceCoroutine이 실행중이라면 코루틴 종료
         if (DeleteByDistanceCoroutine != null)
             StopCoroutine(DeleteByDistance());
-        // Bullet 객체 삭제 
+
         Destroy(gameObject);
     }
 
     // 첫 발사된 위치와 날아가는 Bullet 의 거리를 비교하여 거리가 멀어진다면 Bullet 을 삭제시키고, 코루틴을 멈추기 위한 코드   
     IEnumerator DeleteByDistance()
     {
-        // rigidBody 컴포넌트가 null 이 아니라면
+        // rigidBody 컴포넌트가 null 이면 움직일 수 없으므로 반복의 조건으로 사용
         while(rigidBody != null) 
         {
-            // 날아간 거리를 구하기 위해 Vector3.Distance 함수를 통해 값을 구해줌 
+            // 날아간 거리를 구하기 위해 Vector3.Distance 함수를 통해 값을 계산하고 파괴 조건으로 사용 
             float distanceToStartPosition = Vector3.Distance(startPosition, rigidBody.position);
-            // 만약 distanceToStartPosition 값이 Constant.BULLET_DELETE_DISTANCE(15f) 보다 크다면 삭제 시키는 조건
+            // 만약 distanceToStartPosition 값이 지정값(Constant.BULLET_DELETE_DISTANCE(15f)) 보다 크다면 삭제 시키는 조건을 줘서 거리가 멀다면 삭제
             if (distanceToStartPosition > Constant.BULLET_DELETE_DISTANCE)
             {
-                // 오브젝트를 삭제 시키는 함수 실행
                 Delete();
-                // 코루틴 멈춤
-                yield break;
             }
             // 계속 거리를 비교하는 것이 아니라 코루틴으로 Constant.BULLET_DELETE_TIME(3f) 만큼 멈춘 후 다시 계산
-            // 즉, 3초 마다 한번씩 계산
             yield return new WaitForSeconds(Constant.BULLET_DELETE_TIME);
         }
     }
 
     private void BulletMove()
     {
-        // rigidBody 컴포넌트가 null 이라면
+        // rigidBody 컴포넌트 null 체크를 통해 함수 접근 차단
         if (rigidBody == null)
-            return; // 리턴
+            return;
 
-        // rigidBody가 null 이 아니라면 bullet은 앞으로 날아가기만 하면 되기 때문에 AddForce로 힘을 가해줌
+        // rigidBody가 null 이 아니라면 bullet 은 앞으로 날아가기만 하면 되기 때문에 AddForce로 힘을 가해줌
         rigidBody.AddForce(transform.forward * Constant.BULLET_POWER);
         
 
--- BulletShooter.cs
+++ BulletShooter.cs
@@ -11,13 +11,28 @@ public class BulletShooter : MonoBehaviour
     // 생성할 Bullet 의 원본 프리팹
     public Bullet bulletPrefab;
 
-
     public void Shot()
     {
-        // BulletShooter 의 목적은 원본으로 만들어 둔 Bullet의 Prefab을 통해 사본을 생성하는 역할을 하기 때문에 
+        // BulletShooter 의 목적은 원본으로 만들어 둔 Bullet 의 Prefab 을 통해 사본을 생성하는 역할을 하기 때문에 
         // 다른 조건 없이 shot 함수를 통해 다른 조건 없이 사본을 생성하게 설계 
-        Bullet bullet = Instantiate(bulletPrefab, bulletPoint.position, bulletPoint.rotation);
-        // commit 테스트용 주석
+
+        if (bulletPoint != null)
+        {
+            if (bulletPrefab == null)
+            {
+                // bulletPrefab이 null 인 경우, 새로운 프리팹을 생성해서 예외 처리
+                bulletPrefab = CreateNewBulletPrefab();
+            }
+
+            Bullet bullet = Instantiate(bulletPrefab, bulletPoint.position, bulletPoint.rotation);
+        }
     }
 
+    private Bullet CreateNewBulletPrefab()
+    {
+        GameObject newBulletPrefab = new GameObject("BulletPrefab");
+        // 원하는 구성 요소를 newBulletPrefab에 추가
+
+        return newBulletPrefab.GetComponent<Bullet>();
+    }
 }
